04f0b297481bda6afd7d14b9ac2df700c6fadc25
Ivan Zhang <ivanzhangofficial@gmail.com>
2023-02-27 23:31:36 -0500

    create connection between gct and react frontend

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
 import ScreenLayout from "./components/ScreenLayout";
+import axios from "axios";
 
 function App() {
+  const getGCT = async () => {
+    const response = await axios.post(
+      "http://localhost:5000/gct",
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const { code, digraph } = response.data;
+    console.log(code, digraph);
+  };
+
+  getGCT();
   return (
     <ScreenLayout>
       <h1>HI</h1>
